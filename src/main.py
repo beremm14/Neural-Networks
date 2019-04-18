10390d4a055f252debfff2a9c540f3a5298c199a
Emil Berger <beremm14@htlkaindorf.at>
2019-04-18 23:09:05 +0200

    Add: first 'learning' network

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
+from tensorflow import keras
+
+network = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+
+network.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs=[1, 2, 3]
+ys=[2, 4, 6]
+
+network.fit(xs, ys, epochs=1000)
+
+print(network.predict([1000]))
