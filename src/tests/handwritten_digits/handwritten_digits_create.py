cea760d4cb2facf0cbf11feed1c8d4a995db3ff9
Emil Berger <beremm14@htlkaindorf.at>
2019-04-22 00:04:18 +0200

    Works: 1, 2, 3, 4, 5, 9 - Close: 0, 6, 8, - Bad: 7

--- src/tests/handwritten_digits/handwritten_digits_create.py
+++ src/tests/handwritten_digits/handwritten_digits_create.py
@@ -1,26 +1,69 @@
-import tensorflow as tf
+import numpy
+from tensorflow.keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
 
-# Handwritten digits
-mnist = tf.keras.datasets.mnist
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Values between 0 and 1
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape(X_train.shape[0], 784).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 784).astype('float32')
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(units='128', activation='relu'),
-    #tf.keras.layers.Dense(units='128', activation='relu'),
-    tf.keras.layers.Dense(units='10', activation='softmax')
-])
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255.0
+X_test = X_test / 255.0
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
 
-model.fit(x_train, y_train, epochs=5)
+# define baseline model
+def baseline_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+	# Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-val_loss, val_acc = model.evaluate(x_test, y_test)
-print(val_loss, val_acc)
+# define the larger model
+def larger_model():
+	# create model
+	model = Sequential()
+	model.add(Conv2D(30, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Conv2D(15, (3, 3), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.2))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu'))
+	model.add(Dense(50, activation='relu'))
+	model.add(Dense(num_classes, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
 
-model.save('models/num_reader.model')
+# build the model
+model = baseline_model()
+# Fit the model
+model.fit(X_train, y_train, epochs=20)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Large CNN Error: %.2f%%" % (100-scores[1]*100))
+
+model.save("models/num_reader.model")
--- src/tests/handwritten_digits/handwritten_digits_run.py
+++ src/tests/handwritten_digits/handwritten_digits_run.py
@@ -10,17 +10,19 @@ from skimage import io
 
 model = tf.keras.models.load_model('models/num_reader.model')
 
-# Load pictures for testing
+try:
+    path = abspath('src/pictures/9.png')
+    print(path)
+    img = cv2.imread(path)
+    if img == None:
+        raise Exception('No image')
+except Exception as ex:
+    print(str(ex))
 
-path = abspath('src/pictures/2.jpg')
-print(path)
-img = io.imread(path, as_gray=True)
 
 img = cv2.resize(img, (28, 28))
-IMG = np.reshape(img, (1, 784))
-
-#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-x = image.img_to_array(IMG, dtype=np.float32)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+x = image.img_to_array(img, dtype=np.float32)
 
 if np.any(x[0,0]>250):
     print("Inverting the picture")
@@ -28,11 +30,23 @@ if np.any(x[0,0]>250):
     x *= -1
 
 x = x/255.0
+x = x.reshape(1, 784)
 
 plt.imshow(img, cmap='Greys_r')
-plt.show()
+#plt.show()
 
 numbers = model.predict(x)
 plt.bar(range(10), numbers[0])
-plt.show()
-print('Prediction: Number ', np.argmax(numbers[0]))
+#plt.show()
+print('\nPrediction: Number ', np.argmax(numbers[0]), '\n')
+print('Predictions:')
+print('0: ', numbers[0][0])
+print('1: ', numbers[0][1])
+print('2: ', numbers[0][2])
+print('3: ', numbers[0][3])
+print('4: ', numbers[0][4])
+print('5: ', numbers[0][5])
+print('6: ', numbers[0][6])
+print('7: ', numbers[0][7])
+print('8: ', numbers[0][8])
+print('9: ', numbers[0][9])
