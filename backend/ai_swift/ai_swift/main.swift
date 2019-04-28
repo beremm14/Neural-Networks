fe560afcc3943cf3609543010665a7351fd0a4e3
Emil Berger <beremm14@htlkaindorf.at>
2019-04-28 16:54:27 +0200

    Init Swift Project

---
+++ backend/ai_swift/ai_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ai_swift
+//
+//  Created by Emil Berger on 28.04.19.
+//  Copyright © 2019 Emil Berger. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
