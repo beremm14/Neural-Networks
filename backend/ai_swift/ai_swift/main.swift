87ad6e59301e6e0acb7046eb36d54227f3ce90b6
Emil Berger <beremm14@htlkaindorf.at>
2019-04-29 22:16:40 +0200

    Add: CoreML Project

---
+++ backend/ai_swift/ai_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ai_swift
+//
+//  Created by Emil Berger on 29.04.19.
+//  Copyright © 2019 Emil Berger. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
