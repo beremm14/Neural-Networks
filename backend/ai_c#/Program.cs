a3e8ba36d362cce57e3e04fe641b31e2b2401529
Emil Berger <beremm14@htlkaindorf.at>
2019-05-03 21:50:36 +0200

    Add C# Projects

---
+++ backend/ai_c#/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ai {
+    class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+}
---
+++ theoretical/NeuralNetwork_C#/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NeuralNetwork
+{
+    class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+}
